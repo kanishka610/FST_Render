337658ed07567260de808df03f74b4dee891ed2a
kanishka610 <148204797+kanishka610@users.noreply.github.com>
2025-10-08 23:11:56 +0530

    Delete backend directory

--- backend/models/Activity.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const activitySchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: String
-});
-
-module.exports = mongoose.model('Activity', activitySchema);
